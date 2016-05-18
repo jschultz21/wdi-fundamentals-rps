a1c851d8c66c9d4646b1757e0328d368f8106f49
Julia Schultz <julia.schultz91@gmail.com>
2016-05-18 19:09:48 -0400

    trying to get this thing up on github, yo.

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -4,8 +4,8 @@
 'use strict';
 
 function getInput() {
-    console.log("Please choose either 'rock', 'paper', or 'scissors'.")
-    return prompt();
+    console.log("Please choose either 'rock', 'paper', or 'scissors'.");
+    return prompt("Do you choose rock, paper, or scissors?");
 }
 function randomPlay() {
     var randomNumber = Math.random();
@@ -40,50 +40,92 @@ function getWinner(playerMove,computerMove) {
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
+ 
+
+    if ((computerMove==="rock" && playerMove==="paper") || (computerMove==="paper" && playerMove==="scissors") || (computerMove==="scissors" && playerMove==="rock")) {
+        winner="Human wins!";
+    }
     
-if (playerMove===computerMove) {
-    winner="tie";
-}    
-
-else if (computerMove==="rock" && playerMove==="scissors") {
-    winner="computer";
-}
-
-else if (computerMove==="rock" && playerMove==="paper") {
-    winner="player";
-}
-else if (computerMove==="paper" && playerMove==="scissors") {
-    winner="player";
-}
-
-else if (computerMove==="paper" && playerMove==="rock") {
-    winner="computer";
-}
-
-else if (computerMove==="scissors" && playerMove==="rock") {
-    winner="player";
-}
-
-else if (computerMove==="scissors" && playerMove==="paper") {
-    winner="computer";
-}
-
-else { 
-    winner="Error";
-}
-
-    return winner;
-}
-
-console.log(" winner: " + getWinner('rock', 'rock')); 
+    else if ((computerMove==="rock" && playerMove==="scissors")|| (computerMove==="scissors" && playerMove==="paper") || (computerMove==="paper" && playerMove==="rock")) {
+        winner="Computer wins!";
+    }
+    
+    else if (playerMove==computerMove) {
+        winner="It\'s a tie!";
+    }
+    
+    else { 
+        winner="Error";
+    }
+        return winner;
+    
+    }
 
 
-function playToFive() {
-    console.log("Let's play Rock, Paper, Scissors");
+var playToFive = function() {
+    console.log('Let\'s play Rock Paper Scissors');
     var playerWins = 0;
     var computerWins = 0;
-    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
+
+    while ((playerWins<5) && (computerWins<5)) {
+    var playerMove = getPlayerMove();
+    var computerMove = getComputerMove();
+    
+    console.log('Human played ' + playerMove + ' while computer played ' + computerMove+ '...');
+    console.log(winner=getWinner(playerMove, computerMove));
+
+    if (winner ==="Human wins!"){
+        playerWins+=1;
+    }
+    
+    else if (winner ==="Computer wins!"){
+        computerWins+=1;
+    }
+    else {
+    }
+    
+    console.log("Human has "+playerWins+" and computer has "+ computerWins+".");
+    
+    }
+    console.log("Final score: Human has "+playerWins+" and computer has "+ computerWins+".");
     return [playerWins, computerWins];
-}
+
+};
+
+var playTo = function(x) {
+console.log('Let\'s play Rock Paper Scissors');
+var playerWins = 0;
+var computerWins = 0;
+
+    while ((playerWins<x) && (computerWins<x)) {
+        var playerMove = getInput();
+        var computerMove = randomPlay();
+        
+        console.log('Human played ' + playerMove + ' while computer played ' + computerMove+ '...');
+        console.log(winner=getWinner(playerMove, computerMove));
+
+        if (winner ==="Human wins!"){
+            playerWins+=1;
+        }
+        
+        else if (winner ==="Computer wins!"){
+            computerWins+=1;
+        }
+        else {
+
+        }
+        
+        console.log("Human has "+playerWins+" and computer has "+ computerWins+".");
+        
+    }
+
+console.log("Final score: Human has "+playerWins+" and computer has "+ computerWins+".");
+    return [playerWins, computerWins];
+};
+
+//getPlayerMove();
+//getComputerMove();
+//getWinner('rock','scissors');
+//playToFive();
+//playTo(3);
 
